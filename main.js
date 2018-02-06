cacf376202d82023d7ba766d6cbd8da552be2487
Morten Fæster <faester@gmail.com>
2018-02-06 01:19:19 +0100

    Loading changes etc.

--- main.js
+++ main.js
@@ -1,17 +1,52 @@
 var Userservicejppoldk = require('userservicejppoldk');
 var authorize = require('./authorize.js');
+var program = require('commander');
 
-var authEndpoint = "https://testauth.jppol.dk/connect/token";
+program
+	.version('0.1.0')
+	.option('-i, --id [value]', 'auth.jppol.dk client id')
+	.option('-s, --secret [value]', 'auth.jppol.dk client secret')
+	.option('-A, --auth [value]', 'Authorization endpoint')
+	.option('-U, --userservice [value]', 'Userservice endpoint')
+	.option('-m, --minChange [value]', 'Minimum change number', parseInt)
+	.option('-M, --findMinimum')
+	.parse(process.argv);
 
-var clientId = "d4849a8f1e6245b4b74bd520f4159b3d";
-var clientSecret = "YqEhP9qYK5tFImpekbhPmc3szf+sw50Ic29/24VaBgk=";
+if (typeof localStorage === "undefined" || localStorage === null) {
+  var LocalStorage = require('node-localstorage').LocalStorage;
+  localStorage = new LocalStorage('./scratch');
+}
+
+parameters = ['id', 'secret', 'auth', 'userservice', 'minChange'];
+
+parameters.forEach(function(p) {
+if (program[p] != null) {
+       	console.log (p,'found on command line, setting to', program[p]);	
+	localStorage.setItem(p, program[p]); 
+} else {
+	if (localStorage.getItem(p) === null) {
+		console.error(p, 'missing from command line and not found in local storage');
+		throw 'wrong config'
+	}
+}
+});
+
+
+
+var auth = localStorage.getItem('auth'); 
+var userservice = localStorage.getItem('userservice'); 
+var clientId = localStorage.getItem('id'); 
+var clientSecret = localStorage.getItem('secret');
+var minChange = localStorage.getItem('minChange');
+
 var token = "empty";
-authorize.init(authEndpoint, clientId, clientSecret);
+authorize.init(auth, clientId, clientSecret);
+authorize.authorize();
 
 var Userservicejppoldk = require('userservicejppoldk');
 
 var defaultClient = Userservicejppoldk.ApiClient.instance;
-defaultClient.basePath = "https://userservicetest.jppol.dk";
+defaultClient.basePath = userservice;
 console.log("defaultClient.basePath", defaultClient.basePath);
 
 // Configure OAuth2 access token for authorization: clientAccess
@@ -20,14 +55,14 @@ authorize.register(clientAccess);
 
 var lookupApi = new Userservicejppoldk.LookupAndValidateApi()
 authorize.register(clientAccess);
-authorize.authorize();
 
 
 var l = 1;
-var r = 128;
-var expandingRange = true;
-var findMin = true;
-var minChangeNumber = 1; // {Number} First change of interest
+var r = 12800000;
+var expandingRange = program.findMinimum;
+var findMin = program.findMinimum;
+var retrieving = false;
+var minChangeNumber = minChange; // {Number} First change of interest
 
 var callback = function(error, data, response) { 
 	if (error) { 
@@ -44,11 +79,12 @@ var callback = function(error, data, response) {
 			if (data.length > 128) {
 				r = r * 2;
 			} else {
+				minChangeNumber = r;
+				r = r * 2;
 				expandingRange = false;
 			}
-			minChangeNumber = parseInt((r + l) / 2, 10);
 			console.log("Expanding range", r);
-			lookupApi.getChanges(minChangeNumber, callback); 
+			lookupApi.getChanges(r, callback); 
 		} else if (findMin) {
 			if (data.length >= 128){
 				l = minChangeNumber;
@@ -58,16 +94,20 @@ var callback = function(error, data, response) {
 				minChangeNumber += data.length - 1;
 				findMin = false;
 				console.log("Found min change number", minChangeNumber);
-				setInterval(function() { lookupApi.getChanges(minChangeNumber, callback); }, 10000);
 				return;
 			}
 
 			minChangeNumber = parseInt((r + l) / 2, 10);
 			console.log("Searching", l, "to", r, "size", r - l);
 			lookupApi.getChanges(minChangeNumber, callback); 
+			localStorage.setItem('minChange', minChangeNumber);
+		} else if (!retrieving) {
+			retrieving = true;
+			setInterval(function() { lookupApi.getChanges(minChangeNumber, callback); }, 5000);
 		} else {
 			data.forEach(function(item) { 
 				minChangeNumber = Math.max(item.OperationNumber, minChangeNumber);
+				localStorage.setItem('minChange', minChangeNumber);
 				console.log(item.Operation, item.OperationNumber, item.UserIdentifier, item.EventTime);
 			});
 			if (data.length > 128){
@@ -77,7 +117,7 @@ var callback = function(error, data, response) {
 	} 
 }; 
 
-setTimeout(function() { lookupApi.getChanges(minChangeNumber, callback); }, 500);
+setTimeout(function() { lookupApi.getChanges(minChangeNumber, callback); }, 1000);
 
 
 console.log("hello world!");
